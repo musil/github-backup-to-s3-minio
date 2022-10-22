f57e46412af4346545eb27bf0a568e40d5caaf66
Stanislav Musil <musil@users.noreply.github.com>
2022-10-22 22:55:51 +0200

    feat: cleanup

--- github-to-s3-backup.sh
+++ github-to-s3-backup.sh
@@ -19,8 +19,11 @@ echo "Downloading repositories for" $BACKUP_GITHUB_OWNER
 gh repo list $BACKUP_GITHUB_OWNER --json "name" --limit 1000 --template '{{range .}}{{ .name }}{{"\n"}}{{end}}' | xargs -L1 -I {} gh repo clone $BACKUP_GITHUB_OWNER/{} /tmp/$BACKUP_BUCKET_NAME/{}
 
 echo "Downloaded repositories..."
-find  . -maxdepth 1 -type d
+find  /tmp/$BACKUP_BUCKET_NAME/ -maxdepth 1 -type d
 
 echo "Uploading to MinIO S3 bucket" $BACKUP_BUCKET_NAME "in region" $BACKUP_S3_REGION
 aws --endpoint-url https://$S3_SERVER s3 sync --region=$BACKUP_S3_REGION . s3://$BACKUP_BUCKET_NAME/github.com/$BACKUP_GITHUB_OWNER/`date "+%Y-%m-%d_%H:%m:%S"`/
 echo "Complete."
+
+# Cleanup
+rm -rf /tmp/$BACKUP_BUCKET_NAME/
